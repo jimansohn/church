97c959d740216c7b7af7774724e2ffd380565e06
Ji Man Sohn <jimanni@Jis-MacBook-Pro.attlocal.net>
2021-11-04 22:58:53 -0400

    changed way to access configuration

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-const config = [
-  {
-    name: 'Before After',
-    path: 'before_after',
-    description:
-      'Simple Bible quiz testing on if one memorizes the order of the books in the Bible by asking several books before/after a certain book.',
-  },
-  {
-    name: 'Retreat Nametag Maker',
-    path: 'nametag',
-    description:
-      'A tool that automatically generate the nametag with given informations and creats PDF file to download for printing.',
-  },
-];
+async function populate() {
+  const configFile = await fetch('./config.json');
+  const config = await configFile.json();
 
-function createCard(project) {}
-
-function populate() {
   main = document.getElementsByTagName('main')[0];
   config.forEach((project) => {
     const link = document.createElement('a');
